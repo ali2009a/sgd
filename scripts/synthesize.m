05174d0ba3d9f7a7a5d5f1bbac26864af33294c8
aarab <ali_arab@sfu.ca>
2021-01-16 13:05:15 -0800

    Finalized data generation with Matlab

--- scripts/synthesize.m
+++ scripts/synthesize.m
@@ -1,4 +1,4 @@
-function  synthesize(m,n,l1,l2,l3,p,z,q,S1p,S2p,f, output_dir)
+function  synthesize(m,n,l1,l2,l3,p,z,q,Sp,f, output_dir)
 %clc; clear variables; close all;
 %m = 100;% number of features
 %n = 500;% number of samples
@@ -8,65 +8,25 @@ function  synthesize(m,n,l1,l2,l3,p,z,q,S1p,S2p,f, output_dir)
 %p = 1/4;% non-sparsity of signal
 %z = 3/4;% non-sparsity of necessary confounders
 %q = 0.05;% rate of noise
-%S1p = 3/4;% distribution mean of 1D Prior Score
-%S2p = 1/2;% distribution mean of 2D Prior Score
+%sp = 3/4;% distribution mean of 1D Prior Score
 %f = 28; % number of functions used
 
 %% Data Generation
-% Creation of Features (p)
-v = datasample(1:m,1*l1+2*l2+3*l3,'Replace',false);
-X = rand(n,m); X = (X>1-p);
-% Effect of Causes (l1&2&3)
-V = -ones(n,l1+l2+l3); y = zeros(n,1);
-for i = 1:l1
-    x = X(:,v(i)); y = y|x;
-    V(:,i) = x;
+X = rand(n,m); X = (X>1-p); y = zeros(n,1); % Features
+v = datasample(1:m,1*l1+2*l2,'Replace',false); V = -ones(n,l1+l2); % Cause
+for i = 1:l1 % Outcome
+    x = X(:,v(i)); V(:,i) = x; y = y|x;
 end
 for i = 1:l2
-    ii = l1+2*i; x = X(:,v(ii-1)).*X(:,v(ii));
-    V(:,l1+i) = x; y = y|x;
+    ii = l1+2*i; x = X(:,v(ii-1)).*X(:,v(ii)); V(:,l1+i) = x; y = y|x;
 end
-for i = 1:l3
-    ii = l1+2*l2+3*i;
-    x = X(:,v(ii-2)).*X(:,v(ii-1)).*X(:,v(ii));
-    V(:,l1+l2+i) = x; y = y|x;
-end
-V1 = [v(1:l1)' v(1:l1)'];
-V2 = sort(reshape(v(l1+1:l1+2*l2),2,[])',2); %#ok<UDIM> %
-% Effect of Confounders (z)
-x = rand(n,1); x = (x>1-z); y = y&x;
-% Effect of Noise (q)
-x = datasample(1:n,round(n*q),'Replace',false); y(x)=1-y(x);
-% Association: Correlation (XX)
-XX = corr(X);
-% 1D Prior
-% px = truncate(makedist('Normal','mu',0.5,'sigma',0.1),0,1);
-% pv = truncate(makedist('Normal','mu',S1p,'sigma',0.1),0,1);
-% S1 = random(px,1,m); S1(v) = random(pv,size(v));
-S1 = 0.1*randn(1,m)+0.5; S1(v) = 0*randn(size(v))+S1p;
-S1(S1>1)=1; S1(S1<0)=0;
-% 2D Prior
-% S2 = zeros(m);
-% for i = 1:m-1
-% for j = i+1:m
-% px = truncate(makedist('Normal','mu',XX(i,j),'sigma',0.1),0,1);
-% S2(i,j) = random(px); S2(j,i) = S2(i,j);
-% end
-% end
-% pv = truncate(makedist('Normal','mu',S2p,'sigma',0.1),0,1);
-% for i = 1:l2
-%     S2(V2(i,1),V2(i,2)) = random(pv);
-% end
-S2 = abs(0*randn(m)+XX);
-for i = 1:l2
-    S2(V2(i,1),V2(i,2)) = 0.1*randn+S2p;
-    S2(V2(i,2),V2(i,1)) = S2(V2(i,1),V2(i,2));
-end
-S2(S2>1)=1; S2(S2<0)=0; S2 = tril(S2,-1)+tril(S2,-1)';
-
-%% Export
-disp(['Prevalences Y: %' num2str(100*nnz(y)/numel(y)) ...
-    ', X: %' num2str(100*nnz(V)/numel(V))])
+V1 = [v(1:l1)' v(1:l1)']; V2 = sort(reshape(v(l1+1:l1+2*l2),2,[])',2);
+x = rand(n,1); x = (x>1-z); y = y&x; % Confounders
+x = datasample(1:n,round(n*q),'Replace',false); y(x)=1-y(x); % Noise
+S1 = 0.1*randn(1,m)+0.5; S1(S1>1)=1; S1(S1<0)=0; S1(v) = Sp; % 1D Prior
+S2 = abs(corr(X)); % 2D Similarity
+disp([num2str(l1) ' single & ' num2str(l2) ' pair causes: '])
+disp(v)
 %x = input('Export to CSV? [1/0]');
 x=1
 mkdir(output_dir)
--- scripts/synthesize_wrapper.m
+++ scripts/synthesize_wrapper.m
@@ -1,187 +1,197 @@
-fprintf("running:\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-n_params=[125 250 500 1000]
-for i = 1: length(n_params)
-    output_dir=sprintf('output/param_n_%d/',n_params(i))
-    synthesize(m,n_params(i),l1,l2,l3,p,z,q,S1p,S2p,f, output_dir);
-    fprintf("executed\n")
+trial_num=10
+
+m_active = 1;% number of features
+n_active = 1;% number of samples
+l1_active = 1;    % number of singular causes
+l2_active = 1;    % number of pair causes
+l3_active = 1; % number of triplet causes
+p_active = 1;% non-sparsity of signal
+z_active = 1;% non-sparsity of necessary confounders
+q_active = 1;% rate of noise
+sp_active = 1;% distribution mean of 1D Prior Score
+
+if n_active
+    fprintf("running:\n")
+    m = 100;% number of features
+    n = 500;% number of samples
+    l1 = 0;    % number of singular causes
+    l2 = 1;    % number of pair causes
+    l3 = 0; % number of triplet causes
+    p = 1/4;% non-sparsity of signal
+    z = 3/4;% non-sparsity of necessary confounders
+    q = 0.05;% rate of noise
+    sp = 3/4;% distribution mean of 1D Prior Score
+    f = 28; % number of functions used
+    n_params=[125 250 500 1000]
+    for i = 1: length(n_params)
+        for trial = 1:trial_num
+            output_dir=sprintf('output/param_n_%d/%d/',n_params(i), trial)
+            synthesize(m,n_params(i),l1,l2,l3,p,z,q,sp,f, output_dir);
+            fprintf("executed\n")
+        end
+    end
 end
 
 
-
-fprintf("initializing...\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-m_params=[2500 5000 10000 20000]
-for i = 1: length(m_params)
-    output_dir=sprintf('output/param_m_%d/',m_params(i))
-    synthesize(m_params(i),n,l1,l2,l3,p,z,q,S1p,S2p,f, output_dir);
-    fprintf("executed\n")
-end
-%exit;
-
-
-
-
-fprintf("initializing...\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-p_params= [1/16 1/8 1/4 1/2]
-params=p_params
-for i = 1: length(params)
-    output_dir=sprintf('output/param_p_%.2f/',params(i))
-    synthesize(m,n,l1,l2,l3,params(i),z,q,S1p,S2p,f, output_dir);
-    fprintf("executed\n")
+if m_active
+    fprintf("initializing...\n")
+    m = 100;% number of features
+    n = 500;% number of samples
+    l1 = 0;    % number of singular causes
+    l2 = 1;    % number of pair causes
+    l3 = 0; % number of triplet causes
+    p = 1/4;% non-sparsity of signal
+    z = 3/4;% non-sparsity of necessary confounders
+    q = 0.05;% rate of noise
+    sp = 3/4;% distribution mean of 1D Prior Score
+    f = 28; % number of functions used
+    m_params=[2500 5000 ]
+    params=m_params
+    for i = 1: length(m_params)
+        for trial=1:trial_num
+            output_dir=sprintf('output/param_m_%d/%d/',params(i), trial)
+            synthesize(params(i),n,l1,l2,l3,p,z,q, sp, f, output_dir);
+            fprintf("executed\n")
+        end
+    end
+    %exit;
 end
 
 
-
-fprintf("initializing...\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-z_params=[1/2 3/4 7/8 15/16]
-params=z_params
-for i = 1: length(params)
-    output_dir=sprintf('output/param_z_%.2f/',params(i))
-    synthesize(m,n,l1,l2,l3,p,params(i),q,S1p,S2p,f, output_dir);
+if p_active
+    fprintf("initializing...\n")
+    m = 100;% number of features
+    n = 500;% number of samples
+    l1 = 0;    % number of singular causes
+    l2 = 1;    % number of pair causes
+    l3 = 0; % number of triplet causes
+    p = 1/4;% non-sparsity of signal
+    z = 3/4;% non-sparsity of necessary confounders
+    q = 0.05;% rate of noise
+    sp = 3/4;% distribution mean of 1D Prior Score
+    f = 28; % number of functions used
+    p_params= [1/16 1/8 1/4 1/2]
+    params=p_params
+    for i = 1: length(params)
+        for trial=1:trial_num
+            output_dir=sprintf('output/param_p_%.2f/%d/',params(i), trial)
+            synthesize(m,n,l1,l2,l3,params(i),z,q, sp,f, output_dir);
+            fprintf("executed\n")
+        end
+    end
 end
 
 
-fprintf("initializing...\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-q_params =[0.025 0.05 0.1 0.2]
-params=q_params
-for i = 1: length(params)
-    output_dir=sprintf('output/param_q_%.2f/',params(i))
-    synthesize(m,n,l1,l2,l3,p,z,q_params(i),S1p,S2p,f, output_dir);
+if z_active
+    fprintf("initializing...\n")
+    m = 100;% number of features
+    n = 500;% number of samples
+    l1 = 0;    % number of singular causes
+    l2 = 1;    % number of pair causes
+    l3 = 0; % number of triplet causes
+    p = 1/4;% non-sparsity of signal
+    z = 3/4;% non-sparsity of necessary confounders
+    q = 0.05;% rate of noise
+    sp = 3/4;% distribution mean of 1D Prior Score
+    f = 28; % number of functions used
+    z_params=[1/2 3/4 7/8 15/16]
+    params=z_params
+    for i = 1: length(params)
+        for trial=1:trial_num
+            output_dir=sprintf('output/param_z_%.2f/%d/',params(i), trial)
+            synthesize(m,n,l1,l2,l3,p,params(i),q,sp,f, output_dir);
+        end
+    end
 end
 
 
-fprintf("initializing...\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-S1p_params =[1/2 3/4 7/8 15/16]
-params=S1p_params
-for i = 1: length(params)
-    output_dir=sprintf('output/param_s1p_%.2f/',params(i))
-    synthesize(m,n,l1,l2,l3,p,z,q,params(i),S2p,f, output_dir);
+if q_active
+    fprintf("initializing...\n")
+    m = 100;% number of features
+    n = 500;% number of samples
+    l1 = 0;    % number of singular causes
+    l2 = 1;    % number of pair causes
+    l3 = 0; % number of triplet causes
+    p = 1/4;% non-sparsity of signal
+    z = 3/4;% non-sparsity of necessary confounders
+    q = 0.05;% rate of noise
+    sp = 3/4;% distribution mean of 1D Prior Score
+    f = 28; % number of functions used
+    q_params =[0.025 0.05 0.1 0.2]
+    params=q_params
+    for i = 1: length(params)
+        for trial = 1:trial_num
+            output_dir=sprintf('output/param_q_%.2f/%d/',params(i), trial)
+            synthesize(m,n,l1,l2,l3,p,z,q_params(i),sp,f, output_dir);
+        end
+    end
 end
 
 
-
-fprintf("initializing...\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-S2p_params =[1/2 3/4 7/8 15/16]
-params=S2p_params
-for i = 1: length(params)
-    output_dir=sprintf('output/param_s2p_%.2f/',params(i))
-    synthesize(m,n,l1,l2,l3,p,z,q,S1p,params(i),f, output_dir);
+if sp_active
+    fprintf("initializing...\n")
+    m = 100;% number of features
+    n = 500;% number of samples
+    l1 = 0;    % number of singular causes
+    l2 = 1;    % number of pair causes
+    l3 = 0; % number of triplet causes
+    p = 1/4;% non-sparsity of signal
+    z = 3/4;% non-sparsity of necessary confounders
+    q = 0.05;% rate of noise
+    sp = 3/4;% distribution mean of 1D Prior Score
+    f = 28; % number of functions used
+    S1p_params =[1/2 3/4 7/8 15/16]
+    params=S1p_params
+    for i = 1: length(params)
+        for trial=1:trial_num
+            output_dir=sprintf('output/param_s1p_%.2f/%d/',params(i), trial)
+            synthesize(m,n,l1,l2,l3,p,z,q,params(i),f, output_dir);
+        end
+    end
 end
 
 
-
-
-fprintf("initializing...\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-l1_params =[0 1 2 3]
-params=l1_params
-for i = 1: length(params)
-    output_dir=sprintf('output/param_l1_%d/',params(i))
-    synthesize(m,n,params(i),l2,l3,p,z,q,S1p,S2p,f, output_dir);
+if l1_active
+    fprintf("initializing...\n")
+    m = 100;% number of features
+    n = 500;% number of samples
+    l1 = 0;    % number of singular causes
+    l2 = 1;    % number of pair causes
+    l3 = 0; % number of triplet causes
+    p = 1/4;% non-sparsity of signal
+    z = 3/4;% non-sparsity of necessary confounders
+    q = 0.05;% rate of noise
+    sp = 3/4;% distribution mean of 1D Prior Score
+    f = 28; % number of functions used
+    l1_params =[0 1 2 3]
+    params=l1_params
+    for i = 1: length(params)
+        for trial=1:trial_num
+            output_dir=sprintf('output/param_l1_%d/%d/', params(i), trial)
+            synthesize(m,n,params(i),l2,l3,p,z,q,sp,f, output_dir);
+        end
+    end
 end
 
 
-fprintf("initializing...\n")
-m = 100;% number of features
-n = 500;% number of samples
-l1 = 0;    % number of singular causes
-l2 = 1;    % number of pair causes
-l3 = 0; % number of triplet causes
-p = 1/4;% non-sparsity of signal
-z = 3/4;% non-sparsity of necessary confounders
-q = 0.05;% rate of noise
-S1p = 3/4;% distribution mean of 1D Prior Score
-S2p = 1/2;% distribution mean of 2D Prior Score
-f = 28; % number of functions used
-l2_params =[0 1 2 3]
-params=l2_params
-for i = 1: length(params)
-    output_dir=sprintf('output/param_l2_%d/',params(i))
-    synthesize(m,n,l1,params(i),l3,p,z,q,S1p,S2p,f, output_dir);
+if l2_active
+    fprintf("initializing...\n")
+    m = 100;% number of features
+    n = 500;% number of samples
+    l1 = 0;    % number of singular causes
+    l2 = 1;    % number of pair causes
+    l3 = 0; % number of triplet causes
+    p = 1/4;% non-sparsity of signal
+    z = 3/4;% non-sparsity of necessary confounders
+    q = 0.05;% rate of noise
+    sp = 3/4;% distribution mean of 1D Prior Score
+    f = 28; % number of functions used
+    l2_params =[0 1 2 3]
+    params=l2_params
+    for i = 1: length(params)
+        for trial=1:trial_num
+            output_dir=sprintf('output/param_l2_%d/%d/',params(i), trial)
+            synthesize(m,n,l1,params(i),l3,p,z,q,sp,f, output_dir);
+        end
+    end
 end
