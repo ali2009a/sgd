119178c98247c5a871547d224177008b76f47712
Ali Arab <aliarab@cedar1.cedar.computecanada.ca>
2021-01-17 18:26:22 -0800

    Add the job run script for matlab code

--- scripts/model_params.sh
+++ scripts/model_params.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=sgd_data_params
+#SBATCH --job-name=sgd_model_params
 #SBATCH --output=/home/aliarab/scratch/sgd/result/model_params/output.log
 #SBATCH --time=1-00:00:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=12G
 #SBATCH --account=def-ester
 
-SLURM_JOB_NAME="sgd_data_params"
+SLURM_JOB_NAME="sgd_model_params"
 source /home/aliarab/miniconda3/bin/activate slice_based
 model="model_params"
 input_path="/home/aliarab/scratch/sgd/sim_data/data_params/n/500/1"
